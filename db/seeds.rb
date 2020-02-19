1f0a7f8b069b7ba046f79f81717e22e458ab9e81
Robin Weidlich <robinweidlich7@gmail.com>
2020-02-19 11:56:55 -0300

    create restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurant = Restaurant.create([{ name: 'Epicure', category: 'french',
+  address: '75008 Paris', phone_number: '12345' }, { name: 'Karolina', category:
+  'brazilian', address: '75008 Rio', phone_number: '54321' },
+  { name: 'Amarelinho', category: 'foodpoisoning', address: '75008 Rio', phone_number: '666' }, { name: 'Belavista', category: 'brazilian',
+  address: '75008 Floripa', phone_number: '67891' }, { name: 'Federica', category: 'italian',
+  address: '75008 Floripa', phone_number: '45678' }])
