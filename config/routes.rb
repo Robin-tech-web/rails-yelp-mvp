6befa5fe5c5b0f881903e2e95e8d51cb22f2b807
Robin Weidlich <robinweidlich7@gmail.com>
2020-02-19 15:07:48 -0300

    show and add restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'restaurants', to: 'restaurants#index'
+
+  get    'restaurants/new',      to: 'restaurants#new', as: :new
+  post   'restaurants',          to: 'restaurants#create'
+
+  get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/:id', to: 'restaurants#show', as: :show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
