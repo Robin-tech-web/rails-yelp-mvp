7ba3e8fa8bd9bfd1ccce7ed05cd6fca188769430
Robin Weidlich <robinweidlich7@gmail.com>
2020-02-20 09:40:14 -0300

    do exercise 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
-  # get 'restaurants', to: 'restaurants#index'
-
-  # get 'restaurants/new', to: 'restaurants#new', as: :new
-  # post 'restaurants', to: 'restaurants#create'
-
-  # get 'restaurants', to: 'restaurants#index'
-  # get 'restaurants/:id', to: 'restaurants#show', as: :show
-
   resources :restaurants, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:new, :create]
   end
-
-  # get 'restaurants/:restaurant_id/reviews', to: 'reviews#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
